8dc75e0fde7b561cbd1bae4dfef3271743e0ad84
Jonathan Bento <jhoesantos926@gmail.com>
2025-05-05 23:07:32 -0300

    Atualiza imagem do topo da página inicial para logo-filos.png

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -26,7 +26,7 @@ const Index = () => {
           transition={{ delay: 0.3, duration: 0.5 }}
         >
           <div className="w-40 h-40 flex items-center justify-center mb-4">
-            <img src="/logo-philos.png" alt="Philos Logo" className="w-full h-full object-contain" />
+            <img src="/logo-filos.png" alt="Logo Filos" className="w-full h-full object-contain" />
           </div>
         </motion.div>
 
